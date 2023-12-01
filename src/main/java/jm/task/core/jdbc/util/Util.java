084a0b10e659c49ad6815317f14799f123e6569f
Your Name <you@example.com>
2023-12-01 12:55:46 +0400

    Work 1.1.5, 1.1.3 JPP

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,53 @@
 package jm.task.core.jdbc.util;
 
+import com.fasterxml.classmate.AnnotationConfiguration;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.hibernate.engine.jdbc.connections.internal.DriverConnectionCreator;
+import org.hibernate.mapping.Property;
 
+
+import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static SessionFactory sessionfactory ;
+    public static SessionFactory getSessionFactory() {
+        if (sessionfactory == null) {
+            try {
+                Configuration configration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, DB_URL);
+                settings.put(Environment.USER, userName);
+                settings.put(Environment.PASS, password);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
+                configration.setProperties(settings);
+                configration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configration.getProperties()).build();
+
+                sessionfactory = configration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionfactory;
+    }
     public static final String DB_URL ="jdbc:mysql://localhost:3306/mydbtest";
     public static final String userName ="root";
     public static final String password ="Guantanama20237654!";
@@ -21,10 +59,14 @@ public class Util {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(DB_URL, userName, password);
             }  catch (SQLException  | ClassNotFoundException e) {
-                e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+                e.printStackTrace();
                 System.out.println("Ошибка SQL !");
             }
         }
         return connection;
     }
+
+
+
+
 }
